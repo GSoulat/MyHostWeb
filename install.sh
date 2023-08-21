92e892a856049f9a91f10ac4ceafe5d4a9cf1cef
GSoulat <gsoulat31@gmail.com>
2023-08-21 14:40:25 +0200

    first commit

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+# Demande du nom de domaine principal
+read -p "Veuillez entrer votre nom de domaine principal (par exemple, berry.ovh) : " domain_name
+
 # 1. Mise à jour d'Ubuntu
+echo "    __  __       _    _           ___          __  _      "  
+echo "   |  \/  |     | |  | |         | \ \        / / | |     "
+echo "   | \  / |_   _| |__| | ___  ___| |\ \  /\  / /__| |__   "
+echo "   | |\/| | | | |  __  |/ _ \/ __| __\ \/  \/ / _ \ '_ \  "
+echo "   | |  | | |_| | |  | | (_) \__ \ |_ \  /\  /  __/ |_) | "
+echo "   |_|  |_|\__, |_|  |_|\___/|___/\__| \/  \/ \___|_.__/  "
+echo "            __/ |                                         "
+echo "           |___/                                          "
+
+
 echo "Mise à jour d'Ubuntu..."
 sudo apt update && sudo apt upgrade -y
 
-# 2. Vérification de l'installation de Docker
+# 2. Vérification de l'installation de Docker et Docker Compose
 if ! command -v docker &> /dev/null; then
     echo "Docker n'est pas installé. Installation en cours..."
     sudo apt install -y docker.io
@@ -14,26 +27,29 @@ else
     echo "Docker est déjà installé."
 fi
 
-# 3. Installation de Portainer Business Edition
-echo "Installation de Portainer Business Edition..."
-sudo docker volume create portainer_data
-sudo docker run -d -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest
-
-# 4. Installation de Nginx Proxy Manager
-echo "Installation de Nginx Proxy Manager..."
-sudo docker run -d -p 80:80 -p 81:81 -p 443:443 --name nginx -v /etc/nginx/data:/data -v /etc/nginx/letsencrypt:/etc/letsencrypt jc21/nginx-proxy-manager:latest
-
-# 5. Configuration de Nginx Proxy Manager pour les noms de domaine
-if [ -f DomainName.csv ]; then
-    echo "Fichier DomainName.csv trouvé. Configuration de Nginx Proxy Manager..."
-    IFS=";"
-    while read -r container domain; do
-        # Ici, vous devrez utiliser l'API ou une autre méthode pour configurer Nginx Proxy Manager.
-        echo "Ajout de la configuration pour le domaine $domain pointant vers le conteneur $container"
-    done < DomainName.csv
+if ! command -v docker-compose &> /dev/null; then
+    echo "Docker Compose n'est pas installé. Installation en cours..."
+    sudo apt install -y docker-compose
 else
-    echo "Fichier DomainName.csv non trouvé."
+    echo "Docker Compose est déjà installé."
 fi
 
-echo "Installation terminée."
+# Création du réseau Docker si ce n'est pas déjà fait
+if ! sudo docker network ls | grep -q 'myhost_network'; then
+    echo "Création du réseau Docker 'myhost_network'..."
+    sudo docker network create myhost_network
+else
+    echo "Le réseau Docker 'myhost_network' existe déjà."
+fi
 
+# 3. Démarrage de Portainer avec Docker Compose
+echo "Démarrage de Portainer avec Docker Compose..."
+sudo docker-compose -f Docker/portainer-docker-compose.yml up -d
+echo "Portainer démarré. Vous pouvez y accéder à l'adresse suivante : https://portainer.${domain_name}:9443"
+
+# 4. Démarrage de Nginx Proxy Manager avec Docker Compose
+echo "Démarrage de Nginx Proxy Manager avec Docker Compose..."
+sudo docker-compose -f Docker/nginx-db-docker-compose.yml up -d
+echo "Nginx Proxy Manager démarré. Vous pouvez y accéder à l'adresse suivante : http://nginx.${domain_name}:81"
+
+echo "Installation terminée."
