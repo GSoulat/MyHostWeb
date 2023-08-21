c437ecad14eab4e25c5c78d20bdb0e804a23cb2e
GSoulat <gsoulat31@gmail.com>
2023-08-21 15:01:04 +0200

    update path

--- myhostweb.sh
+++ myhostweb.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 domain_name="$1"
+base_dir=$(dirname "$0")
 
 # Démarrage de Portainer avec Docker Compose
 echo "Démarrage de Portainer avec Docker Compose..."
-sudo docker-compose -f Docker/portainer-docker-compose.yml up -d
+sudo docker-compose -f "$base_dir/Docker/portainer-docker-compose.yml" up -d
 echo "Portainer démarré. Vous pouvez y accéder à l'adresse suivante : https://portainer.${domain_name}:9443"
 
 # Démarrage de Nginx Proxy Manager avec Docker Compose
 echo "Démarrage de Nginx Proxy Manager avec Docker Compose..."
-sudo docker-compose -f Docker/nginx-db-docker-compose.yml up -d
-echo "Nginx Proxy Manager démarré. Vous pouvez y accéder à l'adresse suivante : http://nginx.${domain_name}:81"
-
-echo "Installation terminée."
-
+sudo docker-compose -f "$base_dir/Docker/nginx-db-docker-compose.yml" up -d
+echo "Nginx Proxy Manager démarré. Vous pouvez y accéder à l'adresse suivante : http://nginx.${domain_name
