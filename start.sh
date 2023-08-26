ec03542158c1fc238a0ce8b6f8148cd95dfb4685
GSoulat <gsoulat31@gmail.com>
2023-08-26 16:25:06 +0200

    short

--- start.sh
+++ start.sh
@@ -36,52 +36,52 @@ myhostweb_data() {
     wait $!
 
     # 1. Mise à jour d'Ubuntu
-    gum spin --title.foreground $ORANGE --title="Mise à jour de linux..." sudo apt update && sudo apt upgrade && echo "Update is OK" &> /dev/null
+    gum spin --title.foreground $ORANGE --title="Mise à jour de linux..." sudo apt update && sudo apt upgrade
     wait $!
     gum style --foreground $GREEN "Mise à jour effectué"
 
     # 2. Installation de Git
-    gum spin --title.foreground $ORANGE --title="Installation de Git..." sudo apt install git  && echo "Git is OK"  &> /dev/null
+    gum spin --title.foreground $ORANGE --title="Installation de Git..." sudo apt install git
     wait git
 
 
     # 3. Installation de Zip
-    gum spin --title.foreground $ORANGE --title="Installation de Zip..." sudo apt install zip && echo "Zip is OK"  &> /dev/null
+    gum spin --title.foreground $ORANGE --title="Installation de Zip..." sudo apt install zip
     wait zip
 
     # 4. Vérification de l'existence du répertoire MyHostWeb et suppression si nécessaire
     if [ -d "MyHostWeb" ]; then
-        gum spin --title.foreground $ORANGE --title="Le répertoire MyHostWeb existe déjà. Création du dossier. Suppression en cours..." sudo mv MyHostWeb Backup  &> /dev/null
+        gum spin --title.foreground $ORANGE --title="Répertoire MyHostWeb" sudo mv MyHostWeb Backup
         sudo rm -rf MyHostWeb
     fi
 
     # 5. Clonage du projet GitHub
-    gum spin --title.foreground $ORANGE --title="Clonage du repository Github MyHostWeb..." git clone https://github.com/GSoulat/MyHostWeb.git &> /dev/null
+    gum spin --title.foreground $ORANGE --title="Clonage du repository Github MyHostWeb..." git clone https://github.com/GSoulat/MyHostWeb.git
     sleep 5
 
     # 6. Vérification de l'installation de Docker et Docker Compose
-    gum spin --title.foreground $ORANGE --title="Installation de docker..." sudo apt install docker.io -y && wait docker && echo "Docker is OK"  &> /dev/null
-    gum spin --title.foreground $ORANGE --title="Démarrage de docker..." sudo systemctl start docker &> /dev/null
+    gum spin --title.foreground $ORANGE --title="Installation de docker..." sudo apt install docker.io
+    gum spin --title.foreground $ORANGE --title="Démarrage de docker..." sudo systemctl start docker
     wait $!
-    gum spin --title.foreground $ORANGE --title="Enable docker..." sudo systemctl enable docker &> /dev/null
+    gum spin --title.foreground $ORANGE --title="Enable docker..." sudo systemctl enable docker
     wait $!
 
     
     #7. Vérification de  l'installationd de docker compose
 
-    gum spin --title.foreground $ORANGE --title="Docker Compose installation en cours..." sudo apt install -y docker-compose &> /dev/null
+    gum spin --title.foreground $ORANGE --title="Docker Compose installation en cours..." sudo apt install docker-compose
     wait $!
     docker system prune -a -f
     wait $!
 
     # 8. Création du réseau Docker si ce n'est pas déjà fait
-    sudo docker network create myhost_network &> /dev/null
+    sudo docker network create myhost_network
     wait $!
 
 
     volume_name="myhostweb_data"
     if ! docker volume ls -q | grep -q "^${volume_name}$"; then
-        docker volume create myhostweb_data &> /dev/null
+        docker volume create myhostweb_data
         wait $!
         if docker volume ls -q | grep -q "^${volume_name}$"; then
             gum style --foreground $GREEN "Le volume myhostweb_data à été crée"
