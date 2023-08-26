f2a91eb8f4e3ff765f76788706454ce5895336a7
GSoulat <gsoulat31@gmail.com>
2023-08-26 16:26:48 +0200

    update with time

--- start.sh
+++ start.sh
@@ -33,28 +33,28 @@ gum style \
 
 myhostweb_data() {
     gum style --foreground 4 "Etape 1 : Mise à jour et installation"
-    wait $!
+    sleep 5
 
     # 1. Mise à jour d'Ubuntu
     gum spin --title.foreground $ORANGE --title="Mise à jour de linux..." sudo apt update && sudo apt upgrade
-    wait $!
+    sleep 5
     gum style --foreground $GREEN "Mise à jour effectué"
 
     # 2. Installation de Git
     gum spin --title.foreground $ORANGE --title="Installation de Git..." sudo apt install git
-    wait git
+    sleep 5
 
 
     # 3. Installation de Zip
     gum spin --title.foreground $ORANGE --title="Installation de Zip..." sudo apt install zip
-    wait zip
+    sleep 5
 
     # 4. Vérification de l'existence du répertoire MyHostWeb et suppression si nécessaire
     if [ -d "MyHostWeb" ]; then
         gum spin --title.foreground $ORANGE --title="Répertoire MyHostWeb" sudo mv MyHostWeb Backup
         sudo rm -rf MyHostWeb
     fi
-
+    sleep 5
     # 5. Clonage du projet GitHub
     gum spin --title.foreground $ORANGE --title="Clonage du repository Github MyHostWeb..." git clone https://github.com/GSoulat/MyHostWeb.git
     sleep 5
@@ -62,27 +62,27 @@ myhostweb_data() {
     # 6. Vérification de l'installation de Docker et Docker Compose
     gum spin --title.foreground $ORANGE --title="Installation de docker..." sudo apt install docker.io
     gum spin --title.foreground $ORANGE --title="Démarrage de docker..." sudo systemctl start docker
-    wait $!
+    sleep 5
     gum spin --title.foreground $ORANGE --title="Enable docker..." sudo systemctl enable docker
-    wait $!
+    sleep 5
 
     
     #7. Vérification de  l'installationd de docker compose
 
     gum spin --title.foreground $ORANGE --title="Docker Compose installation en cours..." sudo apt install docker-compose
-    wait $!
+    sleep 5
     docker system prune -a -f
-    wait $!
+    sleep 5
 
     # 8. Création du réseau Docker si ce n'est pas déjà fait
     sudo docker network create myhost_network
-    wait $!
+    sleep 5
 
 
     volume_name="myhostweb_data"
     if ! docker volume ls -q | grep -q "^${volume_name}$"; then
         docker volume create myhostweb_data
-        wait $!
+        sleep 5
         if docker volume ls -q | grep -q "^${volume_name}$"; then
             gum style --foreground $GREEN "Le volume myhostweb_data à été crée"
         fi
@@ -96,7 +96,7 @@ myhostweb_data() {
 
 bye_data() {
     echo "Bonne journée"
-    sleep 60
+    sleep 6
     clear
 }
 
